fd84386d48914e4f0fc5e3522c3b908440f451f9
GIRI <1amgiri@users.noreply.github.com>
2025-05-09 11:34:49 +0530

    New portfolio from scratch using HTML, CSS, and JS

---
+++ script.js
@@ -0,0 +1,34 @@
+const roles = ["Developer", "Programmer", "Data Engineering Enthusiast"];
+let currentRole = 0;
+let charIndex = 0;
+let typing = true;
+
+const typedText = document.querySelector(".typed-text");
+
+function typeEffect() {
+  if (typing) {
+    if (charIndex < roles[currentRole].length) {
+      typedText.textContent += roles[currentRole].charAt(charIndex);
+      charIndex++;
+      setTimeout(typeEffect, 100);
+    } else {
+      typing = false;
+      setTimeout(typeEffect, 2000);
+    }
+  } else {
+    if (charIndex > 0) {
+      typedText.textContent = roles[currentRole].substring(0, charIndex - 1);
+      charIndex--;
+      setTimeout(typeEffect, 50);
+    } else {
+      typing = true;
+      currentRole = (currentRole + 1) % roles.length;
+      setTimeout(typeEffect, 500);
+    }
+  }
+}
+typeEffect();
+
+function toggleMenu() {
+  document.getElementById("nav-links").classList.toggle("show");
+}
